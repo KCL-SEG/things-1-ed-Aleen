cc6c6010f46380badd026c5442b580b804f21fad
Shicheng Li <shicheng.li@kcl.ac.uk>
2023-10-09 11:15:58 +0100

    ypppp

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(blank=True, max_length=120)  # Limiting to 120 characters
+    description = models.TextField(blank=True, max_length=121)  # Limiting to 120 characters
     quantity = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(100)]
     )
